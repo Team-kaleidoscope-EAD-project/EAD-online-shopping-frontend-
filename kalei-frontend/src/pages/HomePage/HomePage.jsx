99abadebf27584a7bb2615588c169a26102e076c
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-10-31 22:10:51 +0530

    commented navbar

--- kalei-frontend/src/pages/HomePage/HomePage.jsx
+++ kalei-frontend/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Navbar from "../../components/Navbar/Navbar";
 import styles from "../HomePage/HomePage.module.css";
 
 export default function HomePage() {
   return (
     <div className={styles.homepageSection}>
       {/* navbar section */}
-      <Navbar />
+
       {/* navbar section */}
       {/* your section */}
 
