ace58bd505a7b96cd1532ac25c92f3175044289c
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-11-12 21:06:34 +0530

    carousel done

--- kalei-frontend/src/pages/HomePage/HomePage.jsx
+++ kalei-frontend/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../HomePage/HomePage.module.css";
+import Carousel from "./Sections/Carousel/Carousel";
 
 export default function HomePage() {
   return (
@@ -8,7 +9,7 @@ export default function HomePage() {
 
       {/* navbar section */}
       {/* your section */}
-
+      <Carousel />
       {/* your section */}
       {/* footer section */}
 
