de49d92bbfe42a3a4e2657a759b2f8844f0e8ff1
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-11-12 22:43:13 +0530

    Collections added to home page

--- kalei-frontend/src/pages/HomePage/HomePage.jsx
+++ kalei-frontend/src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "../HomePage/HomePage.module.css";
+import Collections from "./Sections/Collections/Collections";
 
 export default function HomePage() {
   return (
     <div className={styles.homepageSection}>
       {/* navbar section */}
-
       {/* navbar section */}
       {/* your section */}
-
+      <Collections />
       {/* your section */}
       {/* footer section */}
 
