2bc818a080011940bc435e8a34b08d04a0f6e588
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-11-17 23:57:07 +0530

    product filters
    have to make it responsive

--- kalei-frontend/src/Main.jsx
+++ kalei-frontend/src/Main.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
+// pages
 import HomePage from "./pages/HomePage/HomePage";
+import ProductCatalog from "./pages/HomePage/ProductCatalog";
+// pages
 import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/product-catalog",
+    element: <ProductCatalog />,
     errorElement: <NotFoundPage />,
   },
 ]);
