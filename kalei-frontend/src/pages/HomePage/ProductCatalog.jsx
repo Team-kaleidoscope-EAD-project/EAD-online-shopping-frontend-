b73ead862b8ab6d6973a6d5ac68363556a04260c
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-11-18 15:02:51 +0530

    changes to navbar and filters
    navbar mega menus are on hovers and filter drawers are made responsive

--- kalei-frontend/src/pages/HomePage/ProductCatalog.jsx
+++ kalei-frontend/src/pages/HomePage/ProductCatalog.jsx
@@ -19,9 +19,17 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 // accordian
 
+// drawer
+import Drawer from "@mui/material/Drawer";
+// drawer
+
 // slider
 import Slider from "@mui/material/Slider";
 // slider
+
+// close img
+import closeBtn from "../../assets/images/icons/cross.png";
+// close img
 
 import { grey } from "@mui/material/colors";
 
@@ -46,6 +54,14 @@ const marks = [
 // price ranges
 
 export default function ProductCatalog() {
+  // drawer
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+  // drawer
+
   // price range
   const [val, setVal] = React.useState(MIN);
   const handleChange = (_, newValue) => {
@@ -84,9 +100,13 @@ export default function ProductCatalog() {
             </Breadcrumbs>
           </Stack>
         </Grid>
-        <Grid size={{ xs: 3 }} paddingLeft={3}>
+        <Grid
+          size={{ xs: 10, sm: 6, md: 4, lg: 3 }}
+          paddingLeft={3}
+          sx={{ display: { xs: "none", md: "block" } }}
+        >
           {/* product categories */}
-          <Accordion sx={{ boxShadow: "none", border: "none" }}>
+          <Accordion sx={{ boxShadow: "none", border: "none" }} defaultExpanded>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1-content"
@@ -224,6 +244,193 @@ export default function ProductCatalog() {
             </AccordionDetails>
           </Accordion>
           {/* product price */}
+        </Grid>
+        <Grid
+          size={{ xs: 12 }}
+          sx={{
+            display: { xs: "flex", md: "none" },
+            height: "50px",
+            paddingLeft: "20px",
+            alignItems: "center",
+            justifyContent: { xs: "center", sm: "start" },
+          }}
+          marginTop={-2}
+        >
+          <button className={styles.filtersButton} onClick={toggleDrawer(true)}>
+            Show Filters
+          </button>
+          <Drawer open={open} onClose={toggleDrawer(false)}>
+            <Grid
+              size={{ xs: 12 }}
+              paddingLeft={3}
+              paddingTop={5}
+              paddingRight={5}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              <Grid container paddingBottom={4}>
+                <Grid
+                  size={{ xs: 12 }}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <span className={styles.drawerText}>Filter products</span>
+                  <div
+                    className={styles.drawerCloseBtn}
+                    onClick={toggleDrawer(false)}
+                  >
+                    <img src={closeBtn} alt="close btn" width={20} />
+                  </div>
+                </Grid>
+              </Grid>
+              {/* product categories */}
+              <Accordion
+                sx={{ boxShadow: "none", border: "none" }}
+                defaultExpanded
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                  sx={{ borderBottom: "solid grey 1px" }}
+                >
+                  <span className={styles.accordionText}>Categories</span>
+                </AccordionSummary>
+                <AccordionDetails sx={{ textAlign: "start" }}>
+                  <div className={styles.productCategories}>
+                    <span>All Products</span>
+                    <span>Foot wear</span>
+                    <span>Watches</span>
+                    <span>Denim wear</span>
+                    <span>Men</span>
+                    <span>Women</span>
+                    <span>Pants</span>
+                    <span>Oversized T-shirts</span>
+                    <span>Casuals</span>
+                    <span>Sports wear</span>
+                  </div>
+                </AccordionDetails>
+              </Accordion>
+              {/* product categories */}
+              {/* product sizes */}
+              <Accordion sx={{ boxShadow: "none", border: "none" }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                  sx={{ borderBottom: "solid grey 1px" }}
+                >
+                  <span className={styles.accordionText}>Sizes</span>
+                </AccordionSummary>
+                <AccordionDetails sx={{ textAlign: "start" }}>
+                  <div className={styles.productSizes}>
+                    <div className={`${styles.selectedSizeBox}`}>
+                      <span>All</span>
+                    </div>
+                    <div className={styles.sizeBox}>
+                      <span>XXS</span>
+                    </div>
+                    <div className={styles.sizeBox}>
+                      <span>XS</span>
+                    </div>
+                    <div className={styles.sizeBox}>
+                      <span>S</span>
+                    </div>
+                    <div className={styles.sizeBox}>
+                      <span>M</span>
+                    </div>
+                    <div className={styles.sizeBox}>
+                      <span>L</span>
+                    </div>
+                    <div className={styles.sizeBox}>
+                      <span>XL</span>
+                    </div>
+                    <div className={styles.sizeBox}>
+                      <span>XXL</span>
+                    </div>
+                  </div>
+                </AccordionDetails>
+              </Accordion>
+              {/* product sizes */}
+              {/* product availability */}
+              <Accordion sx={{ boxShadow: "none", border: "none" }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                  sx={{ borderBottom: "solid grey 1px" }}
+                >
+                  <span className={styles.accordionText}>Availability</span>
+                </AccordionSummary>
+                <AccordionDetails sx={{ textAlign: "start" }}>
+                  <div className={styles.checkBoxContainer}>
+                    <Checkbox
+                      sx={{
+                        color: grey[800],
+                        "&.Mui-checked": {
+                          color: grey[900],
+                        },
+                      }}
+                      defaultChecked
+                      name="availability"
+                    />
+                    <span>In Stock</span>
+                  </div>
+                  <div className={styles.checkBoxContainer}>
+                    <Checkbox
+                      sx={{
+                        color: grey[800],
+                        "&.Mui-checked": {
+                          color: grey[900],
+                        },
+                      }}
+                      name="availability"
+                    />
+                    <span>Out Of Stock</span>
+                  </div>
+                </AccordionDetails>
+              </Accordion>
+              {/* product availability */}
+              {/* product price */}
+              <Accordion sx={{ boxShadow: "none", border: "none" }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                  sx={{ borderBottom: "solid grey 1px" }}
+                >
+                  <span className={styles.accordionText}>Price</span>
+                </AccordionSummary>
+                <AccordionDetails sx={{ textAlign: "start" }}>
+                  <Slider
+                    marks={marks}
+                    step={10}
+                    value={val}
+                    valueLabelDisplay="auto"
+                    min={MIN}
+                    max={MAX}
+                    onChange={handleChange}
+                    color="grey"
+                    sx={{
+                      color: grey[800],
+                      "&.Mui-checked": {
+                        color: grey[900],
+                      },
+                    }}
+                  />
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <span className={styles.sliderText}>Rs. {MIN}</span>
+                    <span className={styles.sliderText}>Rs. {MAX}</span>
+                  </Box>
+                </AccordionDetails>
+              </Accordion>
+              {/* product price */}
+            </Grid>
+          </Drawer>
         </Grid>
       </Grid>
       {/* product catalog */}
